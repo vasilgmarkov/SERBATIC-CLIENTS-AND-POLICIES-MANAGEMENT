7dbe9178364e667f7278930729c88e0ac4a3d5f9
Vasil Markov <vasilgmarkov@gmail.com>
2020-06-13 12:51:23 +0200

    Add url param to the services getPolicies and getClients

--- services/services.js
+++ services/services.js
@@ -2,11 +2,9 @@ const axios = require("axios");
 /**
  * Get clients from source url
  */
-exports.getClients = async () => {
+exports.getClients = async (url) => {
   try {
-    let res = await axios.get(
-      "http://www.mocky.io/v2/5808862710000087232b75ac"
-    );
+    let res = await axios.get(url);
     let result = res.data.clients;
     return result;
   } catch (error) {
@@ -17,11 +15,9 @@ exports.getClients = async () => {
 /**
  * Get policies from source url
  */
-exports.getPolicies = async () => {
+exports.getPolicies = async (url) => {
   try {
-    let res = await axios.get(
-      "http://www.mocky.io/v2/580891a4100000e8242b75c5"
-    );
+    let res = await axios.get(url);
     let policies = res.data.policies;
     return policies;
   } catch (error) {
